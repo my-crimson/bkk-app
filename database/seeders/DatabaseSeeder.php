1d4eaf91609ade556170478400d0fc0c10813b95
SoraYaki04 <pakli8104@gmail.com>
2026-05-07 01:10:08 +0700

    update seeder users management role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,31 +10,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Seed jurusan
-        $jurusanList = [
-            'Teknik Komputer dan Jaringan',
-            'Rekayasa Perangkat Lunak',
-            'Multimedia',
-            'Teknik Kendaraan Ringan',
-            'Teknik Sepeda Motor',
-            'Teknik Instalasi Tenaga Listrik',
-            'Teknik Pemesinan',
-            'Teknik Pengelasan',
-            'Tata Busana',
-            'Akuntansi dan Keuangan Lembaga',
-        ];
-
-        foreach ($jurusanList as $j) {
-            DB::table('jurusan')->insert(['jurusan' => $j]);
-        }
-
-        // Seed default admin user
-        DB::table('users')->insert([
-            'username' => 'admin',
-            'password' => Hash::make('admin123'),
-            'role' => 'admin',
-        ]);
-
         // Seed management user
         DB::table('users')->insert([
             'username' => 'management',
